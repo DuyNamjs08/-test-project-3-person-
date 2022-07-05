c82b6408fa813c096a7fdbde14e734347b19e839
duynam08 <duynam11a11999@gmail.com>
2022-07-05 16:44:46 +0700

    bai tap thuat toan

--- test.js
+++
@@ -1,6 +0,0 @@
-const arr = [1, 3, 3, 3, 4, 5, 5, 5, 8, 8];
-const count = (arr, val) => {
- return arr.reduce((a, b) => (b === val ? a + 1 : a), 0);
-};
-const result = count(arr, 5);
-console.log("số lần xuất hiện:", result);
