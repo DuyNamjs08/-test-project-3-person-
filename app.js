5a2facc4f30155645212e6f3c53389be0ea2a03d
duynam08 <duynam11a11999@gmail.com>
2022-07-05 16:43:25 +0700

    bai tap

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ const arr = [1, 3, 3, 3, 4, 5, 5, 5, 8,8];
 const arr1 = new Set(arr);
 const arr2=[...arr1]
 const test=(test)=>{
-     const test1= test.sort((a,b)=>{
-             return b-a
+     const test1= test.reduce((a,b)=>{
+             return (a>b) ? a : b 
      })
-     return test1[1]
+     const test2 = test.filter((item) => item != test1);
+    
+     return test2[test2.length - 1];
 }
-console.log(`số lớn nhất trong mảng là :${test(arr2)}`);
+console.log(`số lớn nhì trong mảng là :${test(arr2)}`);
 test(arr2)
-const count = (arr, val) =>
-arr.reduce((a, b) => (b === val ? a + 1 : a), 0);
+
+const count = (arr, val) =>{
+
+        return  arr.reduce((a, b) => (b === val ? a + 1 : a), 0);
+        }
 const result=count(arr, test(arr2));
 console.log("số lần xuất hiện:", result);
 
@@ -19,7 +24,7 @@ console.log("số lần xuất hiện:", result);
 
 const st="[4,5,6,23,34,55,55,5,6]";
 const newAr=JSON.parse(st)
-const numberMax = newAr.sort((a,b)=>{
-    return a-b
+const numberMax = newAr.reduce((a,b)=>{
+    return (a>b) ? a : b
 });
-console.log(`số lớn nhất trong chuỗi là :${numberMax[numberMax.length -1]}`);
+console.log(`số lớn nhất trong chuỗi là :${numberMax}`);
